d337d5fd1319cb36fc3859dd2093af0740924559
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2021-05-31 16:48:53 -0400

    Improve how docs are built locally
    Cherry picked from stimpool

--- tasks.py
+++ tasks.py
@@ -161,11 +161,11 @@ def tests(c):
 
 @task(
     help={
-        "serve": "Build the docs watching for changes",
-        "open_browser": "Open  the docs in the web browser",
+        "serve": "Build the docs watching for changes (Default=True)",
+        "open_browser": "Open the docs in the web browser (Default=True)",
     }
 )
-def docs(c, serve=False, open_browser=False):
+def docs(c, serve=True, open_browser=True):
     # type: (Context, bool, bool) -> None
     """Build documentation."""
     build_docs = f"sphinx-build -b html {DOCS_DIR} {DOCS_BUILD_DIR}"
